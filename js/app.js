737c3e96c4f90bcbd49c0a941583061ba94dccb1
Noah Asher Smith <bigbass98@gmail.com>
2018-05-18 11:15:37 -0500

    Final Draft?
    Possibly my final draft, will have someone look at it before submitting.

--- js/app.js
+++ js/app.js
@@ -5,3 +5,26 @@
 baguetteBox.run('.gallery', {
     animation: 'fadeIn'
 });
+
+//Search Bar Code
+
+//Define search and input
+const search = document.getElementById("search");
+const img = document.querySelectorAll("a[data-caption]");
+
+//Listener
+ search.addEventListener("keyup", function(event) {
+//Search input
+     let result = search.value.toLowerCase();
+//Display what is searched
+     for (let i = 0; i < img.length; i++){
+        let title = img[i].getAttribute("data-caption");
+         if (title.toLowerCase().includes(result)) {
+             img[i].style.display = '';
+//Display nothing if input is wrong
+         } else {
+              img[i].style.display = 'none';
+            
+         }
+     }
+ });
